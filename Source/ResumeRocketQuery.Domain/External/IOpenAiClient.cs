7537efd13ca824ce0d703eaa635d19a7c7aa8c04
Noot <meganootsupreme@gmail.com>
2024-04-08 21:31:09 -0600

    setup files for open ai implementation

---
+++ Source/ResumeRocketQuery.Domain/External/IOpenAiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeRocketQuery.Domain.External
+{
+    public interface IOpenAiClient
+    {
+        Task<string> SendMessageAsync(string prompt);
+    }
+}
