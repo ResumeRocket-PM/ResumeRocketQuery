12a71b55f88895502d7252b00b9b1b59143f2863
Yinghua Yin <u1297837@umail.utah.edu>
2024-04-08 22:01:56 -0600

    jobscraper setup

---
+++ Source/ResumeRocketQuery.External/jobScraper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeRocketQuery.External
+{
+    public interface IjobScraper
+    {
+
+        Task<string> scrapJobPosting(string url);
+    }
+    public class jobScraper : IjobScraper 
+    {
+        public async Task<string> scrapJobPosting(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
