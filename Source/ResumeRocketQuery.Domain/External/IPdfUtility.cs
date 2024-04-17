a2c1af03c4e4c706a43feb174936b84d390d2463
Nathan Weston <nathan.weston@marketstar.com>
2024-04-17 12:37:07 -0600

    Setup base structure for PDF Utility, and added IronPDF NuGet package to solution

---
+++ Source/ResumeRocketQuery.Domain/External/IPdfUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeRocketQuery.Domain.External
+{
+    public interface IPdfUtility
+    {
+        Task<string> ReadPdfAsync(string filepath);
+        Task<string> UpdatePdfAsync(string filepath);
+    }
+}
