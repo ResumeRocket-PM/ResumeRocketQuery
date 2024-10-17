deac26ee8f147705bf6a2100bddde48ab7884e35
Yinghua Yin <u1297837@umail.utah.edu>
2024-10-16 21:12:46 -0600

    profileContorller, profileService are done

---
+++ Source/ResumeRocketQuery.Domain/Services/IProfileService.cs
@@ -0,0 +1,18 @@
+﻿using ResumeRocketQuery.Domain.Services.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeRocketQuery.Domain.Services
+{
+    public interface IProfileService
+    {
+        Task<List<ProfileResult>> GetUniversity(string input, bool orderType = true);
+        Task<List<ProfileResult>> GetState(string input, bool orderType = true);
+        Task<List<ProfileResult>> GetMajor(string input, bool orderType = true);
+        Task<List<ProfileResult>> GetCareer(string input, bool orderType = true);
+
+    }
+}
